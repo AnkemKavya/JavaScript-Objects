fcc7c0b6106474144584640008827d3891f3a3c6
kavyaankem <ankemkavyaa03@gmail.com>
2026-07-24 18:35:27 +0530

    Objects in array

---
+++ ObjectsInArray/objectsInArray.js
@@ -0,0 +1,14 @@
+let fruitsName = [];
+function onClickArrayObjects() {
+    debugger;
+    let fruits = new Object();
+    fruits.fruitName = document.getElementById("txtFruitName").value;
+    fruits.category = document.getElementById("Category").value;
+    fruits.price = Number(document.getElementById("txtPrice").value);
+    fruits.country = document.getElementById("Country").value;
+    fruits.state = document.getElementById("State").value;
+    fruits.quantity = Number(document.getElementById("txtQuantity").value);
+    fruitsName.push(fruits);
+    console.log(fruitsName);
+    document.getElementById("divFruits").innerHTML = JSON.stringify(fruitsName);
+}
